672c8ca30e5f91225e127e648812a71a1a819c0c
蔡子熙 <caizx15@mails.tsinghua.edu.cn>
2017-11-09 09:39:33 +0800

    fix bug in convert.c and judging a line of zero should be inserted between text section and data section, otherwise, there would be a tiny misalignment between inst addr and inst data in data section. (Maybe a fault of TA?) reduce the JUDGE_CLK, in order to prevent it from skipping sampling a certain testcase.

--- sim/integration_test/convert.c
+++ sim/integration_test/convert.c
@@ -29,38 +29,59 @@ void binary_out(FILE* out,unsigned char* mem)
 
 int main(void)
 {
-	FILE *in;
+	FILE *in1;
+    FILE *in2;
 	FILE *out;
 
 	int i,j,k;
 	unsigned char mem[32];
 
-    in = fopen("main.bin", "rb");
+    in1 = fopen("main.text", "rb");
+    in2 = fopen("main.data", "rb");
     out = fopen("ram_init_data.coe","w");
 
 	fprintf(out, "memory_initialization_radix = 16;\n");
 	fprintf(out, "memory_initialization_vector =\n");
-	while(!feof(in)) {
-	    if(fread(mem,1,4,in)!=4) {
+	while(!feof(in1)) {
+	    if(fread(mem,1,4,in1)!=4) {
 	        fprintf(out, "%02x%02x%02x%02x\n", mem[3], mem[2],	mem[1], mem[0]);
-		break;
-	     }
+		    break;
+	    }
 	    fprintf(out, "%02x%02x%02x%02x\n", mem[3], mem[2], mem[1],mem[0]);
+    }
+    fprintf(out, "00000000\n");
+    while(!feof(in2)) {
+        if(fread(mem,1,4,in2)!=4) {
+            fprintf(out, "%02x%02x%02x%02x\n", mem[3], mem[2],  mem[1], mem[0]);
+            break;
         }
-	fclose(in);
+        fprintf(out, "%02x%02x%02x%02x\n", mem[3], mem[2], mem[1],mem[0]);
+    }
+	fclose(in1);
+    fclose(in2);
 	fclose(out);
 
-    in = fopen("main.bin", "rb");
+    in1 = fopen("main.text", "rb");
+    in2 = fopen("main.data", "rb");
     out = fopen("ram_init_data.mif","w");
 
-	while(!feof(in)) {
-	    if(fread(mem,1,4,in)!=4) {
+	while(!feof(in1)) {
+	    if(fread(mem,1,4,in1)!=4) {
             binary_out(out,mem);
-		break;
-	     }
+		    break;
+	    }
+        binary_out(out,mem);
+    }
+    fprintf(out, "00000000000000000000000000000000\n");
+    while(!feof(in2)) {
+        if(fread(mem,1,4,in2)!=4) {
             binary_out(out,mem);
+            break;
         }
-	fclose(in);
+        binary_out(out,mem);
+    }
+	fclose(in1);
+    fclose(in2);
 	fclose(out);
 
     return 0;
