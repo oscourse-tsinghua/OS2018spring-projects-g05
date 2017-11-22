2b8a34de9556942f2b5f1a119e52b487d169a82c
蔡子熙 <caizx15@mails.tsinghua.edu.cn>
2017-11-22 14:48:08 +0800

    add ports related to ram into devctrl

--- src/devctrl.vhd
+++ src/devctrl.vhd
@@ -16,12 +16,19 @@ entity devctrl is
         devByteSelect_i: in std_logic_vector(3 downto 0);
 
         int_o: out std_logic_vector(IntWidth);
-        timerInt_i: in std_logic
+        timerInt_i: in std_logic;
 
         -- Signals connecting to devices --
         addr_o: out std_logic_vector(AddrWidth);
         int_i: in std_logic_vector(IntWidth);
         timerInt_o: out std_logic;
+
+        -- Signals connecting to ram_ctrl --
+        ramEnable_o: out std_logic;
+        ramReadEnable_o: out std_logic;
+        ramDataSave_o: out std_logic_vector(DataWidth);
+        ramDataLoad_i: in std_logic_vector(DataWidth);
+        ramWriteBusy_i: in std_logic;
 
         -- Signals connecting to flash_ctrl --
         flashEnable_o: out std_logic;
@@ -34,7 +41,7 @@ end devctrl;
 architecture bhv of devctrl is
 begin
 
-    devBusy_o <= flashBusy_i;
+    devBusy_o <= flashBusy_i or ramWriteBusy_i;
 
     process (flashBusy_i) begin
         if (falling_edge(flashBusy_i)) then
@@ -47,11 +54,14 @@ begin
             devBusy_o <= PIPELINE_NONSTOP;
             devDataLoad_o <= (others => '0');
         else
+            ramEnable_o <= DISABLE;
             flashEnable_o <= DISABLE;
-            flashReadEnable_o <= DISABLE;
 
             if (devPhysicalAddr_i <= 32ux"fffff") then
                 -- RAM --
+                ramEnable_o <= ENABLE;
+                ramReadEnable_o <= not devWrite_i;
+                addr_o <= devPhysicalAddr_i;
             elsif (devPhysicalAddr_i = 32ux"f000000") then
                 -- keyboard --
             elsif (devPhysicalAddr_i >= 32ux"1e000000" and devPhysicalAddr_i <= 32ux"1effffff") then
