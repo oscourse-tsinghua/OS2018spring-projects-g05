b515c9d173c1399ead38fa8f5c07c8ba5dd7763e
唐适之 <tsz15@mails.tsinghua.edu.cn>
2017-11-24 21:21:28 +0800

    Fixed byte order conversion
    If we want to convert, we should convert all of the instrution fetching,
    data loading and data saving.
    And we don't need to convert it with our board.

--- src/conv_endian.vhd
+++ src/conv_endian.vhd
@@ -2,6 +2,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity conv_endian is
+    generic (
+        enable: boolean
+    );
     port (
         input: in std_logic_vector(31 downto 0);
         output: out std_logic_vector(31 downto 0)
@@ -10,5 +13,11 @@ end conv_endian;
 
 architecture bhv of conv_endian is
 begin
-    output <= input(7 downto 0) & input(15 downto 8) & input(23 downto 16) & input(31 downto 24);
+    process (all) begin
+        if (enable) then
+            output <= input(7 downto 0) & input(15 downto 8) & input(23 downto 16) & input(31 downto 24);
+        else
+            output <= input;
+        end if;
+    end process;
 end bhv;
