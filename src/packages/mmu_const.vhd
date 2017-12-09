9bbffba96422c8a9f7c6efd6a39c3ba18baa07df
Xihang Liu <liuxh15@mails.tsinghua.edu.cn>
2017-12-09 22:38:07 +0800

    fix a bug in ASID field

--- src/packages/mmu_const.vhd
+++ src/packages/mmu_const.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 use work.global_const.all;
 
 package mmu_const is
-    constant TLB_ENTRY_NUM: integer := 128;
-    subtype TLBIndexWidth is integer range 6 downto 0;
+    constant TLB_ENTRY_NUM: integer := 16;
+    subtype TLBIndexWidth is integer range 3 downto 0;
 
     type TLBEntry is record
         hi, lo0, lo1: std_logic_vector(DataWidth);
