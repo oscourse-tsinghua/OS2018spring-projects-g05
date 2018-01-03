ff2ced531ce0b2507bdda8addcef3aac01dc418b
蔡子熙 <caizx15@mails.tsinghua.edu.cn>
2018-01-03 21:16:13 +0800

    rewrite usb_ctrl.vhd -- remove latch

--- src/usb_ctrl.vhd
+++ src/usb_ctrl.vhd
@@ -22,62 +22,28 @@ entity usb_ctrl is
 end usb_ctrl;
 
 architecture bhv of usb_ctrl is
-    signal weLatch, rdLatch: std_logic;
+    signal state: integer;
     signal readData: std_logic_vector(7 downto 0);
 begin
 
     usbA0_o <= addr_i(2);
     usbRst_o <= not rst;
     usbCS_o <= not devEnable_i;
-    usbWE_o <= (not devEnable_i) and (not writeEnable_i) and weLatch;
-    usbRD_o <= (not devEnable_i) and (not readEnable_i) and rdLatch;
+    usbWE_o <= (not devEnable_i) and (not writeEnable_i) and (state >= 2) and (state <= 4);
+    usbRD_o <= (not devEnable_i) and (not readEnable_i);
     usbData_io <= writeData_i(7 downto 0);
     int_o <= usbInt_i;
     usbDACK_o <= '1';
 
     readData_o <= 24ux"0" & readData;
+    busy_o <= PIPELINE_STOP when state /= 5 and devEnable_i = ENABLE else PIPELINE_NONSTOP;
 
-    process (clk)
-        variable state: integer := 0;
-    begin
+    process(clk) begin
         if (rising_edge(clk)) then
             if (rst = RST_ENABLE or devEnable_i = DISABLE) then
-                state := 0;
-                busy_o <= '0';
-                readData <= (others => '0');
+                state <= 1;
             else
-                if (state = 4) then
-                    state := 0;
-                    busy_o <= '0';
-                    if (readEnable_i = ENABLE) then
-                        readData <= usbData_io;
-                    end if;
-                else
-                    if (state = 0) then
-                        busy_o <= '1';
-                    end if;
-                    state := state + 1;
-                end if;
-            end if;
-        end if;
-    end process;
-
-    process (clk)
-        variable state: integer := 0;
-    begin
-        if (falling_edge(clk)) then
-            if (rst = RST_ENABLE or devEnable_i = DISABLE) then
-                state := 0;
-                weLatch <= '0';
-                rdLatch <= '0';
-            else
-                state := state + 1;
-                if (state = 1) then
-                    weLatch <= '1';
-                    rdLatch <= '1';
-                elsif (state = 4) then
-                    weLatch <= '0';
-                end if;
+                state <= state + 1;
             end if;
         end if;
     end process;
