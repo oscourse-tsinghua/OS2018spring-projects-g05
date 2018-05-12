cd661b4e4dcba031f835dd71319c158e66874547
XemboLiu <Xembo_Liu@outlook.com>
2018-05-11 21:29:40 -0700

    cp0_config registers specification
    for config 0:
    	31: 1
    	30-16: zero because we do not need an fixed mapping MMU
    	BE: 0(for little endian)
    	AT: 0(mips32)
    	AR: 0(release 1)
    	MT(9:7): 1(because we are standard TLB)
    	VI: 0(for instruction cache is not cache)
    	K0(2:0): 2 for Uncached and 3 for Cacheable, writing other value will be ignored
    	Only bits 2:0, 30:25 can be write(inclusive)
    	**we can only write 2 or 3 to 2:0**
    
    for config1:
    	31: 1
    	30-25: MMU Size - 1 (001111)
    	24:22: I-Cache sets per way (001 - for 128 sets per way)
    	21:19: I-cache line size (011 - for 16 bytes)
    	18:16: I-cache associativity (000 - for direct map)
    	15:13: D-cacge set per way (001 - for 128 sets per way)
    	12:10: D-cache line size (011 - for 16 bytes)
    	9:7: D-cache associativity (000 - for direct map)
    	6: 0(for coprocess2 not implemented)
    	5: 0(for MDMX not implemented, note: MDMX is MIPS Digital Media Extension)
    	4: 1(for performance counter registers implemented)
    	3: 0(for watch registers not implemented)
    	2: 0(for MIPS16e not implemented)
    	1: 0(for EJTAG not implemented)
    	0: 0(for floating point unit not implemented)
    	None is writable
    
    for config 2(we can directly initialize as 0x80000000):
    	31: 1
    	30-28: Implementation-specific teriary cache control
    	27-24: Tertiary cache sets per way(I think we can set it to zero)
    	23:20: Tertiary cache line size, should be zero
    	19:16: Tertiary cache associativity
    	15:12: implementation-specific cache control
    	11:8: Second cache sets per way
    	7:4: Secondary cache line size
    	3:0 Secondary cache associativity
    	Note: though 30:28(TU) and 15:12(SU) are market R/W in document, we must ignore this because we doesn't implemented related mechanisms
    
    for config 3(directly initialize as 0x00000000):
    	31: zero
    	-- config4 is generally optional, so we do not need to set it as 1
    	30: zero
    	29: zero(for Coherency Manager memory-mapped Global Configuration Register space is not implemented)
    	28: zero(for MIPS SIMD Architecture is implemented)
    	27: zero(for BadInstrP register not implemented)
    	26: zero(for BadInstr register not implemented)
    	25: zero(for cp0_SegCtl series registers not implemented)
    	24: zero(for page table walk cp0_PW series registers not implemented)
    	23: Zero: for Virtualization Module not implemented
    	22:21: IPLW: (0 for status_IPL 6-bits width, and 1 for 8-bits width)
    	20:18: zero(for microMIPS32 not implemented)
    	17: zero(for MIPS MCU ASE not implemented)
    	16: zero(for MIPS32 is used on entrance to an exception vector)
    	15:14: zero(for only mips32 instruction set is implemented)
    	13: zero(for UserLocal register not implemented)
    	12: zero(for RIE and XIE not implemented)
    	11: zero(for DSP2P not implemented)
    	10: zero(for MIPS DSP module not implemented, note: DSP is digital signal processing)
    	9: zero(for ContextConfig register not implemented)
    	8: zero(for IFlowTrace not implemented)
    	7: zero(for Large Physical Address not implemented)
    	6: zero(should return zero on read in release 1)
    	5: zero(should return zero on read in release 1)
    	4: zero(for Small page support not implemented)
    	3: zero(for device memory map not implemented)
    	2: zero(for Mt Module not implemented, note: unlike others, I don't find a proper meaning for MT)
    	1: zero(for SmartMIPS not implemented)
    	0: zero(for Trace Logic not implemented)

--- src/packages/ddr3_const.vhd
+++ src/packages/ddr3_const.vhd
@@ -9,7 +9,6 @@ package ddr3_const is
     subtype BurstLenWidth is integer range BURST_LEN_WIDTH - 1 downto 0;
     type BurstDataType is array(0 to (BURST_LEN - 1)) of std_logic_vector(31 downto 0);
 
-
     constant OFFSET_WIDTH: integer := BURST_LEN_WIDTH;
     constant INDEX_WIDTH: integer := 4;
     constant TAG_WIDTH: integer := 25 - OFFSET_WIDTH - INDEX_WIDTH;
