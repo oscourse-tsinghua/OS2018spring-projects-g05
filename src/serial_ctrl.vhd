b09b75253715b306e3291b4d9be5a7977a2fd950
roastduck <beantang.tang@gmail.com>
2018-05-27 12:45:40 +0800

    Add TX_IRE and RX_IRE bit to serial_ctrl

--- src/serial_ctrl.vhd
+++ src/serial_ctrl.vhd
@@ -24,16 +24,23 @@ end serial_ctrl;
 architecture bhv of serial_ctrl is
     signal recvAvail: std_logic;
     signal recvData: std_logic_vector(7 downto 0);
+    signal txIRE, rxIRE: std_logic; -- Interrupt Enable
+    constant TX_READY: integer := 0;
+    constant RX_READY: integer := 1;
+    constant TX_IRE: integer := 2;
+    constant RX_IRE: integer := 3;
 begin
-    dataLoad_o <=
-            (1 => rxdReady_i or recvAvail, 0 => not txdBusy_i, others => '0')
-        when mode_i = '1' else
+    dataLoad_o <= (
+            TX_READY => not txdBusy_i,
+            RX_READY => rxdReady_i or recvAvail,
+            TX_IRE => txIRE,
+            RX_IRE => rxIRE,
+            others => '0'
+        ) when mode_i = '1' else
             24ux"0" & rxdData_i when rxdReady_i = '1' else 24ux"0" & recvData;
     -- When recvAvail = NO or chip disabled, outputting whatever is OK
 
-    int_o <= recvAvail;
-    -- Do NOT use rxdReady_i. If data comes when we are handling an exception,
-    -- we must get acknowledged after returing to user mode.
+    int_o <= ((rxdReady_i or recvAvail) and rxIRE) or (not txdBusy_i and txIRE);
 
     process (clk) begin
         if (rising_edge(clk)) then
@@ -42,19 +49,27 @@ begin
             if (rst = RST_ENABLE) then
                 recvAvail <= NO;
                 recvData <= (others => '0');
+                txIRE <= NO;
+                rxIRE <= YES;
             else
                 if (rxdReady_i = '1') then
                     recvAvail <= YES;
                     recvData <= rxdData_i;
-                elsif (enable_i = ENABLE and readEnable_i = ENABLE and mode_i = '0') then
+                end if;
+                if (enable_i = ENABLE and readEnable_i = ENABLE and mode_i = '0') then
                     recvAvail <= NO;
                     recvData <= (others => '0');
                 end if;
 
-                if (enable_i = ENABLE and readEnable_i = DISABLE and txdBusy_i = '0') then
-                    -- If busy, ignore it
-                    txdStart_o <= '1';
-                    txdData_o <= dataSave_i(7 downto 0);
+                if (enable_i = ENABLE and readEnable_i = DISABLE) then
+                    if (mode_i = '1') then
+                        txIRE <= dataSave_i(TX_IRE);
+                        rxIRE <= dataSave_i(RX_IRE);
+                    elsif (txdBusy_i = '0') then -- mode_i = '0'
+                        -- If busy, ignore it
+                        txdStart_o <= '1';
+                        txdData_o <= dataSave_i(7 downto 0);
+                    end if;
                 end if;
             end if;
         end if;
